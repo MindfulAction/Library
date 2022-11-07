144527711b55d0de2556faa9f6a4f711d2ead87f
John Kelly <jpkjpk98@me.com>
2022-11-06 19:38:51 -0800

    Create Book constructor function + related functions

--- javascript.js
+++ javascript.js
@@ -1,13 +1,36 @@
+// Need to create displayBooks function & table elements for HTML that will display the books
+
+// Constructor function for creating new book
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.getInfo = function() {
-      const info = `${title} by ${author}, ${pages} pages,    ${read}`;
+      const info = `${title} by ${author}, ${pages} pages, ` + checkIfRead();
       return info;
     }
   }
+
+  function checkIfRead () {
+    let status;
+    if (this.read) {
+      status = "has been read";
+    } else {
+      status = "has NOT been read";
+    }
+    return status;
+  }
   
-  let theHobbit = new Book("The Hobbit", "J.R.R Tolkein", 295, "not read yet");
-  console.log(theHobbit.getInfo())
+  function addBookToLibrary (title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+  }
+
+  // function displayBooks {
+  //   myLibrary.forEach(book => {
+
+  //   })
+  // }
+
+  let myLibrary = [];
